89768eabdad2370bea884b2666b7be8a122ed845
Lizhou <lisie31s@gmail.com>
2023-08-13 00:37:23 +0800

    feat: add base model class (base cylinder)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,31 +3,33 @@
 </template>
 
 <script>
-  import { ThreeEngine } from './ts/TEngine.ts'
-  import { allBaseObject } from './ts/TBaseObject.ts'
-  import { allLights } from './ts/TLights.ts'  
-  import { allHelper } from './ts/THelper.ts'
-  export default {
-    name: 'HelloWorld',
-    data() {
-      return {
-        ThreeEngine: null,
-      };
-    },
-    mounted() {
-      this.ThreeEngine = new ThreeEngine(this.$refs.threeTarget)
-      this.ThreeEngine.addObject(...allBaseObject)  // add basic model
-      this.ThreeEngine.addObject(...allLights)  // add light
-      this.ThreeEngine.addObject(...allHelper)  // add support helper
-    }
+import { ThreeEngine } from './ts/TEngine.ts'
+import { allBaseObject } from './ts/TBaseObject.ts'
+import { cylinder_base_model, BaseModel } from './ts/TBaseModel.ts'
+import { allLights } from './ts/TLights.ts'
+import { allHelper } from './ts/THelper.ts'
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      ThreeEngine: null,
+    };
+  },
+  mounted() {
+    this.ThreeEngine = new ThreeEngine(this.$refs.threeTarget)
+    let test_base_model = new BaseModel(new cylinder_base_model([0.5, 5.49, 5.49, 14.48, 5.19, 5.19]))
+    this.ThreeEngine.addObject(...test_base_model.allBaseObject)
+    this.ThreeEngine.addObject(...allLights)  // add light
+    this.ThreeEngine.addObject(...allHelper)  // add support helper
   }
+};
 </script>
 
 <style scoped>
-  .three-canvas {
-    width: 100vh;
-    height: 100vh;
-    overflow: hidden;
-    background-color: #d6eaff;
-  }
+.three-canvas {
+  width: 100vh;
+  height: 100vh;
+  overflow: hidden;
+  background-color: #d6eaff;
+}
 </style>
--- src/components/ts/TBaseModel.ts
+++ src/components/ts/TBaseModel.ts
@@ -1,23 +1,73 @@
-import { Mesh } from "three"
+import { Mesh, CylinderGeometry, MeshPhongMaterial } from "three"
 
 interface model_base {
-    construct_Mesh(): Mesh;
+    construct_Mesh(): Array<Mesh>;
 }
 
+export class cylinder_base_model implements model_base {
 
+    CylinderArray: Array<CylinderGeometry> = [];
+    params: Array<number> = [];
+    constructor(params: Array<number>) {
+        this.params = params
+        let index: number = 0;
+        if (params.length > 0) {
+            while (index < params.length) {
+                let CylinderHeight = params[index++]
+                let CylinderTopRadius = params[index++]
+                let CylinderButtomRadius = params[index++]
+                this.CylinderArray.push(
+                    new CylinderGeometry(CylinderTopRadius, CylinderButtomRadius, CylinderHeight, 32)
+                        .translate(0, this.caculate_height(CylinderHeight), 0))
 
-export class BaseModel{
+            }
+        }
+        else {
+            // TODO : Sphere
+        }
+    }
 
+    caculate_height(current_height: number): number {
+        let height: number = 0;
+        for (let i = 0; i < this.CylinderArray.length; i++) {
+            // console.log(this.params[i*3])
+            height += this.params[i * 3]
+        }
+        return height + 0.5 * (current_height)
+    }
 
-    count : number = 1;
-    param : Array<number> = [];
-    all_objects : Array<Mesh> = [];
+    construct_Mesh() {
+        let allBaseObject: Array<Mesh> = [];
+        this.CylinderArray.forEach(cylindergeometry => {
+            allBaseObject.push(new Mesh(
+                cylindergeometry
+                , new MeshPhongMaterial({
+                    color: 'rgb(36,172,242)',
+                    transparent: true,
+                    opacity: 0.80
+                })))
+        })
+        return allBaseObject;
+    }
+}
 
-    constructor(count : number, param : Array<number>){
+export class import_model implements model_base {
+    // TODO : 用以引入的 Model
 
-        this.count = count
-        this.param = param
-
+    construct_Mesh() {
 
     }
+}
+
+export class BaseModel {
+
+    model_type: model_base;
+    allBaseObject: Array<Mesh>;
+
+    constructor(model_type: model_base) {
+
+        this.model_type = model_type
+        this.allBaseObject = this.model_type.construct_Mesh()
+    }
+
 }
