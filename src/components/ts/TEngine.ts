a440cb040dd15db721a6c186db39888ffe36bf72
Lizhou <lisie31s@gmail.com>
2023-08-16 16:33:10 +0800

    fix: click event and move event are conflict.

--- src/components/ts/TEngine.ts
+++ src/components/ts/TEngine.ts
@@ -10,14 +10,18 @@ export class ThreeEngine {
     constructor(dom: any, basemodel: BaseModel) {
 
         // create renderer
-        let renderer = new WebGLRenderer({
+        const renderer = new WebGLRenderer({
             antialias: true, // 抗鋸齒
         })
 
+        let current_mouse = { x: Number, y: Number }
+
+        let mousemove: boolean = false
+
         dom.appendChild(renderer.domElement) // 掛載抗鋸齒
         renderer.setSize(dom.offsetWidth, dom.offsetHeight, true)
-        let scene = new Scene()
-        let camera = new PerspectiveCamera(45, dom.offsetWidth / dom.offsetHeight, 1, 1000)
+        const scene = new Scene()
+        const camera = new PerspectiveCamera(45, dom.offsetWidth / dom.offsetHeight, 1, 1000)
         camera.position.set(50, 50, 50)
         camera.lookAt(new Vector3(0, 0, 0))
         camera.up = new Vector3(0, 1, 0)
@@ -25,17 +29,12 @@ export class ThreeEngine {
         renderer.setClearColor('rgb(195,234,245)')
 
         /* 軌道控制器 */
-        let cameracontrol = new OrbitControls(camera, renderer.domElement)
-        cameracontrol.mouseButtons = {      // 取消滑鼠左鍵用途
-            LEFT: null,
-            MIDDLE: MOUSE.DOLLY,
-            RIGHT: MOUSE.ROTATE
-        }
+        const cameracontrol = new OrbitControls(camera, renderer.domElement)
 
         /* 射線，用於建立滑鼠點擊功能 */
-        let raycaster = new Raycaster()
+        const raycaster = new Raycaster()
 
-        let mouse = new Vector2()
+        const mouse = new Vector2()
         let mouseX = 0; let mouseY = 0; let mouseWidth = 0; let mouseHeight = 0;
 
         renderer.domElement.addEventListener("mousemove", event => {
@@ -46,28 +45,53 @@ export class ThreeEngine {
 
             mouse.x = mouseX / mouseWidth * 2 - 1
             mouse.y = -mouseY * 2 / mouseHeight + 1
+            mousemove = true;
         })
 
-        renderer.domElement.addEventListener("click", () => {
+        function onMouseClick() {
             raycaster.setFromCamera(mouse, camera)
             const intersection = raycaster.intersectObjects(scene.children)
             // console.log(intersection)
             try {
                 if (intersection[0].object.geometry.type === "SphereGeometry") {
-                    let point = intersection[0].object.geometry.boundingSphere.center;
-                    let v = basemodel.search_points(point.x, point.y, point.z, 10)
+                    const point = intersection[0].object.geometry.boundingSphere.center;
+                    const v = basemodel.search_points(point.x, point.y, point.z, 10)
                     camera.position.set(v.x, v.y, v.z)
                     cameracontrol.target.set(point.x, point.y, point.z,)
                 }
             }
             catch (e) {
-
+                // TODO
             }
+        }
 
-        })
+        function onMouseUp(event) {
+            if (mousemove) {
+                // console.log("Mouse Dragged");
+                if (current_mouse != null) {
+                    const distance = Math.sqrt(Math.pow(event.clientX - current_mouse.x, 2) + Math.pow(event.clientY - current_mouse.y, 2));
+                    if (distance < 10) {
+                        // console.log("{INFO} Moving less than 10 px");
+                        onMouseClick();
+                    }
+                }
+            } else {
+                onMouseClick();
+            }
+        }
+
+        function onMouseDown(event) {
+            current_mouse.x = event.clientX
+            current_mouse.y = event.clientY
+            mousemove = false
+        }
+
+        renderer.domElement.addEventListener('mouseup', onMouseUp);
+        renderer.domElement.addEventListener('mousedown', onMouseDown);
+
 
         /* 渲染 */
-        let animate = () => {
+        const animate = () => {
             renderer.render(scene, camera)
             cameracontrol.update()
             requestAnimationFrame(animate)
